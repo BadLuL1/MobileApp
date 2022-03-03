ab1b59ba59466b125f626514204d48f6753a14d0
BadLuL1 <m.derya2001@hotmail.com>
2022-03-03 13:51:03 +0300

    Mobile app first step

---
+++ app/src/main/java/com/h5200042/hkdtic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.h5200042.hkdtic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
