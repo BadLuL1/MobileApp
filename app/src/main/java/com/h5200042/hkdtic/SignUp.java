90cc505787ff7de684dbd9957d3714a87a254a5b
BadLuL1 <m.derya2001@hotmail.com>
2022-03-10 23:22:14 +0300

    Mobile app second step

---
+++ app/src/main/java/com/h5200042/hkdtic/SignUp.java
@@ -0,0 +1,14 @@
+package com.h5200042.hkdtic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
