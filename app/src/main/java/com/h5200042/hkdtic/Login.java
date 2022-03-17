65b2f8b5f6e1ae8694a29c669200a4210f737925
BadLuL1 <m.derya2001@hotmail.com>
2022-03-17 22:53:48 +0300

    Mobile app third step

---
+++ app/src/main/java/com/h5200042/hkdtic/Login.java
@@ -0,0 +1,43 @@
+package com.h5200042.hkdtic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        Button buttonSignUp = findViewById(R.id.btn_uye_ol);
+
+        buttonSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(this,SignUp.class);
+
+            startActivity(intent);
+        });
+
+        Button buttonForgetPassword = findViewById(R.id.btn_sifremi_unuttum);
+
+        buttonForgetPassword.setOnClickListener(v -> {
+            Intent intent2 = new Intent(this, ChooseScreenResetPass.class);
+
+            startActivity(intent2);
+        });
+
+        Button buttonHome = findViewById(R.id.btn_giris_yap);
+
+        buttonHome.setOnClickListener(v -> {
+            Intent intent3 = new Intent(this, Home.class);
+
+            startActivity(intent3);
+        });
+
+    }
+
+}
